e2582871d9f0b35d4cb37e3063e260b3e5d1e652
Bryan Casler <bryan.casler@gmail.com>
2020-05-25 09:23:56 -0400

    Everything is working except @use in SASS is not compiled

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,13 +15,13 @@ module.exports = merge(common, {
         use: [
           "style-loader", //4. Inject CSS into DOM
           "css-loader", // 3. From css to vanilla js
-          {
-            loader: "postcss-loader", // 2. Add Autoprefixer to CSS
-            options: {
-              ident: "postcss",
-              plugins: [require("autoprefixer")]
-            }
-          },
+          // {
+          //   loader: "postcss-loader", // 2. Add Autoprefixer to CSS
+          //   options: {
+          //     ident: "postcss",
+          //     plugins: [require("autoprefixer")]
+          //   }
+          // },
           "sass-loader" //1. From SASS to CSS
         ]
       }
